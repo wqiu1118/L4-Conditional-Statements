f20c9b16bd546ee6b35d598de9befa40e180f996
WenkaiQiu <107879837+wqiu1118@users.noreply.github.com>
2022-06-21 22:03:17 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Conditional Statements 
+Used for decision-making operation based on the condition. 
+
+## Condition 
+An expression that evaluates to produce a result which is a Boolean value (T/F)
+
+```python
+if (CONDITION): 
+  BODY STATEMENT
+
+else: 
+  BODT STATEMENT
+```
+
+Python relies on indentation to define scope 
---
+++ main.py
@@ -0,0 +1,31 @@
+isHungry = input("Y/N: Are you hungry? ")
+isBored = input("Y/N: Are you bored? ")
+if (isHungry == "Y" or isHungry == "y"):
+  print("Go eat")
+
+else: 
+  print("Don't eat")
+if(isBored == "Y" or isBored == "y"): 
+  print ("Go to sleep")
+else: 
+  print("Do nothing")
+
+userNumber = int(input("Give me a number: "))
+if(userNumber >= 0):
+  print("Your number is positive")
+else:  
+  print("Your number is negative") 
+
+#Ask the user for their age. 
+#If the user is older than 17, let them know they can watch any movies.
+#If they are younger than 17 but older than 13, let them know they can watch G-rated and PG13
+#If they are younger than 13, they are only allowed to watch G-rated movies
+
+userAge = int(input("Enter your age: "))
+
+if(userAge >= 17):
+  print("You can watch all movies!")
+elif(userAge <17 and userAge >= 13): 
+  print("You can watch G-rated and PG-13 movies!")
+else:
+  print("You can only watch G-rated movies.")
